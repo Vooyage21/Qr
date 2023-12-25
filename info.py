d923e5478f0274ec5f17b808b79ac4fec68029a8
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2023-12-26 03:02:41 +0700

    Update info.py

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '28284211'))
-API_HASH = environ.get('API_HASH', '6b0afc140b8534c45de9dc77b7d37415')
-BOT_TOKEN = environ.get('BOT_TOKEN', "5634679884:AAH6KjE4JbS5rD3ljyZSJZJx6I-rbz5WYG4")
+API_ID = int(environ.get('API_ID', '2'))
+API_HASH = environ.get('API_HASH', '6b0af5')
+BOT_TOKEN = environ.get('BOT_TOKEN', "5634679")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
